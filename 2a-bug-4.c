2776c6a0c5e04ec316a4466e5f81eb2e229623f4
Julieanna97 <kisamae1997@gmail.com>
2022-10-24 14:25:59 +0200

    Fixed the issue by reading the current file.

--- 2a-bug-4.c
+++ 2a-bug-4.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 
-
 int main() 
 {
 	printf("#include <stdio.h>\n");
 	printf("int main()\n");
 	printf("{\n");
         /* uuuhhh */
-	printf("}\n");
+	printf("}\n"); 
+
+	FILE *fpointer;
+	char ch;
+
+	fpointer = fopen(__FILE__, "r");
+
+	if(!fpointer)
+	{
+		printf("File is empty!\n");
+	}
+
+	for(int i = 0; ch != EOF; i++)
+	{
+		ch = fgetc(fpointer);
+		printf("%c", ch);
+	}
+
+	if(feof(fpointer))
+	{
+		printf("You have reached the end of the file.\n");
+	}
+
+	fclose(fpointer);
+
 	return(0);
 }
