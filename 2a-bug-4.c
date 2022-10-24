34cbb54deba75193224e5e4828b9ee471d0c2713
Nicklnt <116169364+Nicklnt@users.noreply.github.com>
2022-10-24 12:43:50 +0200

    Update 2a-bug-4.c
    fixad, det behövs lite om \"

--- 2a-bug-4.c
+++ 2a-bug-4.c
@@ -6,7 +6,10 @@ int main()
 	printf("#include <stdio.h>\n");
 	printf("int main()\n");
 	printf("{\n");
+    printf("printf(\"include <stdio.h>\");\n");
+    printf("printf(\"int main()\");\n");
         /* uuuhhh */
 	printf("}\n");
+    printf("return(0);");
 	return(0);
 }
