fa54f3d60db27017bc33afa2fb88c337f40a0ebd
Frowsty <daniel.barbis@frostys.tech>
2022-10-24 11:30:58 +0200

    Program that will print itself out

--- 2a-bug-4.c
+++ 2a-bug-4.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-
 int main() 
 {
-	printf("#include <stdio.h>\n");
-	printf("int main()\n");
-	printf("{\n");
-        /* uuuhhh */
-	printf("}\n");
+	char c;
+	FILE *fp = fopen(__FILE__, "r");
+	while (c != EOF)
+	{
+		c = fgetc(fp);
+		putchar(c);
+	}
+
+	fclose(fp);
 	return(0);
 }
