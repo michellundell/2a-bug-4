c5a4e6f52f6ad9d14d77992705165ee93def4f17
Wayazaexa <joakimenglund@protonmail.com>
2022-10-24 11:24:36 +0200

    Finished writing the program that writes itself to stdout.

--- 2a-bug-4.c
+++ 2a-bug-4.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-
-int main() 
+int main()
 {
-	printf("#include <stdio.h>\n");
-	printf("int main()\n");
-	printf("{\n");
-        /* uuuhhh */
-	printf("}\n");
+	char line[80];
+	FILE *fp = fopen(__FILE__, "r");
+
+	while (fgets(line, 80, fp) != NULL)
+	{
+		printf("%s", line);
+	}
+	fclose(fp);
+	
 	return(0);
 }
